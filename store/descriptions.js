626161bcacf8bb88bced267762da3a80754b4a89
Jekabs <jekabs.hincenbergs@gmail.com>
2022-07-20 20:10:44 +0300

    changes

--- store/descriptions.js
+++ store/descriptions.js
@@ -1,17 +1,17 @@
 export const state = () => ({
     descriptions: [
-        {
-            "text": "\n\nLorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups",
-            "index": 0,
-            "logprobs": null,
-            "finish_reason": "length"
-        },
-        {
-            "text": "\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-            "index": 0,
-            "logprobs": null,
-            "finish_reason": "length"
-        },
+        // {
+        //     "text": "\n\nLorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups",
+        //     "index": 0,
+        //     "logprobs": null,
+        //     "finish_reason": "length"
+        // },
+        // {
+        //     "text": "\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+        //     "index": 0,
+        //     "logprobs": null,
+        //     "finish_reason": "length"
+        // },
     ]
 })
 
